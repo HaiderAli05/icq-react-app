9b8dd297487b7b28dd8982dc2c3acdd5d3069d8b
Haider Ali <haider.ali3737@gmail.com>
2022-09-09 21:20:33 +0500

    Add aws server URL for API calls in config file.

--- src/api/config.js
+++ src/api/config.js
@@ -1,1 +1,5 @@
-export const API_BASE_URL = 'https://648a-39-35-8-104.ngrok.io';
+// API URL for production (AWS Server)
+export const API_BASE_URL = 'http://ec2-3-144-149-155.us-east-2.compute.amazonaws.com:3005';
+
+// API URL for development (ngrok)
+// export const API_BASE_URL = 'https://648a-39-35-8-104.ngrok.io';
